0296a4c1c60e607a9746c4477f3526254dd343fb
chenjiawei <chenjiawei@hxkid.com>
2017-03-08 23:26:32 +0800

    update 修改页面布局增加歌曲信息

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 [TOC]
-
 # 熟悉前端开发以及vue基础知识
 
 ## 目标：学习Vue + 音乐播放器Demo
@@ -7,14 +6,14 @@
 
 ## 功能点&计划 （03-07 ~ 03-10 4days）
 
-1. 音乐播放功能（03-07） 已完成
+1. 音乐播放功能（03-07）
 2. 音乐信息展示（03-08）
 3. 音乐评论列表展示（03-09）
 4. 模拟用户发送评论（03-10）
 
 
 技术架构：Vue + vue-cli + Bootstrap
-
+代码仓库：https://github.com/adamchenjiawei/imitate_netease_cloud
 额外补充：单元测试&代码走查
 
 ## 学习过程
@@ -148,9 +147,133 @@ npm run dev
 
 ###day2
 
+1. 修复昨天单元测试执行出错问题
+2. 调整播放界面布局 显示歌曲基本信息（标题、歌手、专辑、歌词等信息）
+3. 实现播放按钮控制音频播放
+[nodejs版本更新](http://blog.csdn.net/sruru/article/details/46301405)
+```
+npm 版本更新
+
+npm install -g npm
+
+nodejs 版本更新
+
+1. 检查 Node的当前版本，使用命令
+node -v
+
+2. 清除npm cache
+sudo npm cache clean -f
+
+3. 安装n模块
+sudo npm install -g n
+
+4. 升级到最新版本
+sudo n 0.8.11
+或者你也可以告诉管理器，安装最新的稳定版本
+sudo n stable
+
+```
+
+
+
 
 
 ## 问题
-1、单元测试没有跑起来 （03-07）
+
+### 单元测试执行出错；(03-07)
+**a. 出错信息： 未安装PhantomJS**
+```
+08 03 2017 10:31:58.022:ERROR [launcher]: No binary for PhantomJS browser on your platform.
+  Please, set "PHANTOMJS_BIN" env variable.
+08 03 2017 10:31:58.025:ERROR [karma]: [TypeError: Cannot read property 'stderr' of undefined]
+TypeError: Cannot read property 'stderr' of undefined
+
+```
+
+解决方法：
+
+参考资料：
+[切换npm源](http://blog.csdn.net/fjnjxr/article/details/53462422)
+[安装PhantomJS](http://blog.csdn.net/xuexiiphone/article/details/52233352)
+```
+1. 切换npm源
+npm config set registry http://registry.npm.taobao.org/
+
+2. 安装PhantomJS (翻墙下载安装  有点慢)
+sudo npm install -g karma-phantomjs-launcher
+
+3. 配置环境变量（MacOS）
+vim ~/.bash_profile
+
+增加
+export PHANTOMJS_BIN=/usr/local/lib/node_modules/karma-phantomjs-launcher/node_modules/phantomjs-prebuilt/bin/phantomjs
+
+source ~/.bash_profile
+
+
+```
+
+**b. 出错信息：未安装latest**
+```
+08 03 2017 13:29:37.307:WARN [reporter]: SourceMap position not found for trace: undefined
+PhantomJS 2.1.1 (Mac OS X 0.0.0) ERROR
+  Error: Module build failed: Error: Couldn't find preset "latest" relative to directory "/Users/adam/Documents/private_repo/imitate_netease_cloud"
+
+```
+
+解决方案：
+[参考](https://segmentfault.com/q/1010000006005280/a-1020000006008290)
+[.babelrc](https://zhuanlan.zhihu.com/p/24224107)
+```
+安装latest
+
+sudo npm install -save-dev babel-preset-latest
+```
+
+**c. 出错信息：selenium sever 启动出错**
+```
+Starting selenium server... There was an error while starting the Selenium server:
+
+Exception in thread "main" java.lang.UnsupportedClassVersionError: org/openqa/grid/selenium/GridLauncherV3 : Unsupported major.minor version 52.0
+
+```
+
+解决方法：
+[参考](http://www.cnblogs.com/plus-chen/p/6392251.html)
+
+```
+1. 安装chromedriver
+sudo npm install chromedriver
+
+java版本与selenium支持版本对应
+
+J2SE 8 = 52, J2SE 7 = 51, J2SE 6.0 = 50, J2SE 5.0 = 49, JDK 1.4 = 48, JDK 1.3 = 47, JDK 1.2 = 46, JDK 1.1 = 45
+
+这里报错的是52版本  java版本需要切换成java8的版本
+
+```
+
+**d. 出错信息： Path must be a string. Received null**
+```
+> imitate_netease_cloud@1.0.0 unit /Users/defaultuser/kid/imitate_netease_cloud
+> cross-env BABEL_ENV=test karma start test/unit/karma.conf.js --single-run
+
+08 03 2017 17:03:15.675:ERROR [plugin]: Error during loading "/Users/defaultuser/kid/imitate_netease_cloud/node_modules/karma-phantomjs-launcher" plugin:
+  Path must be a string. Received null
+Hash: 42c9471806d19b9e4455
+Version: webpack 2.2.1
+```
+
+解决方案：
+[参考](https://github.com/karma-runner/karma-phantomjs-launcher/issues/120)
+```
+1. 安装 phantomjs-prebuilt
+sudo npm install  phantomjs-prebuilt --save
+```
+
+
+03-08
+1. 如何引入css文件
+2. vue 如何引入jquery
 
 ## 资料
--- src/components/music/index.vue
+++ src/components/music/index.vue
@@ -1,13 +1,153 @@
 <template>
   <div>
-    <h1>music</h1>
-    <audio controls="controls" src="http://182.140.142.10/m10.music.126.net/20170307193929/c6eb2e29ae37b9bdb283adcd5b365a1e/ymusic/1709/eb2a/f3c3/3e2dbbf1abf1469a7e635b0dd45aa402.mp3?wshc_tag=0&wsts_tag=58be9616&wsid_tag=ded3d550&wsiphost=ipdbm"></audio>
+    <div class="heard">
+
+    </div>
+    <div class="container">
+      <div class="music-info">
+        <div class="cover">
+          <span class="msk"></span>
+          <img src="../../assets/cover.jpeg"/>
+          <audio src="http://oibzvf99m.bkt.clouddn.com/test_music.mp3"></audio>
+        </div>
+        <div class="info">
+          <div class="title">
+            <i class="tag"></i>
+            <div class="title-desc">
+              <h1 class="music-title">なんでもないや (movie ver.)</h1>
+              <p class="desc">动画电影《你的名字。》ED</p>
+            </div>
+          </div>
+          <div class="singer">
+            <p>歌手: RADWIMPS</p>
+            <p>所属专辑：君の名は。</p>
+          </div>
+
+          <div class="control">
+            <input type="button" @click="playMusic" class="play-music" value="播放">
+          </div>
+          <div class="lyrics">
+            <p>
+              二人の間 通り過ぎた風は
+              <br/>
+              掠过两人之间的风
+              <br/>
+              どこから寂しさを運んできたの
+              <br/>
+              捎来不知来处的寂寞
+              <br/>
+              泣いたりしたそのあとの空は
+              <br/>
+              哭泣过后眺望的天空
+              <br/>
+              やけに透き通っていたりしたんだ
+              <br/>
+              有种格外的通透
+              <br/>
+              いつもは尖ってた父の言葉が
+              <br/>
+              平日里刺耳的父亲的话语
+              <br/>
+              今日は暖かく感じました
+              <br/>
+              今天却倍感温暖
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <section class="music-comment">
+
+      </section>
+    </div>
+
   </div>
 </template>
 <style scoped>
+  .heard {
+    width: 100%;
+    height: 30px;
+    background-color: red;
+    position: fixed;
+    top: 0;
+  }
+  .title {
+    display: inline-block;
+  }
+  .title-desc {
+    float: right;
+  }
+  .desc {
+    text-align: left;
+    position: relative;
+    bottom: 25px;
+  }
+  .cover {
+    width: 28%;
+    height: 20px;
+    float: left;
+    position: absolute;
+  }
+  .cover img {
+    top: 48px;
+    position: absolute;
+    left: 48px;
+    z-index: -1;
+  }
+  .info {
+    width: 69%;
+    float: right;
+  }
+  .singer {
+    text-align: left;
+    margin-left: 65px;
+  }
+  .tag {
+    width: 54px;
+    height: 24px;
+    background: url('../../assets/icon.png');
+    display: inline-block;
+    float: left;
+    margin-top: 30px;
+  }
+  .music-info {
+  }
+  .music-title {
+    text-align: left;
+  }
+  .lyrics {
+    text-align: left;
+    margin-left: 60px;
+  }
+  .control {
+    text-align: left;
+    margin-left: 60px;
+  }
+  .msk {
+    width: 206px;
+    height: 205px;
+    background-position: -140px -580px;
+    background-image: url(../../assets/coverall.png);
+    position: absolute;
+    top: 10px;
+    left: 10px;
+  }
 </style>
 
 <script>
   export default {
+    methods: {
+      playMusic () {
+        const audio = document.getElementsByTagName('audio')[0]
+        const playButton = document.getElementsByClassName('play-music')[0]
+        if (audio.paused) {
+          audio.play()
+          playButton.setAttribute('value', '暂停')
+        } else {
+          audio.pause()
+          playButton.setAttribute('value', '播放')
+        }
+      }
+    }
   }
 </script>
